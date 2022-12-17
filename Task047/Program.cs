e236e4001851b59b38c6c840397952895a3687b2
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-17 12:37:20 +0300

    task 47 done

--- Task047/Program.cs
+++ Task047/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+using static Common.Helper;
+
+Console.WriteLine();
+uint m = UintInput("Введите желаемое количество строк массива: ");
+uint n = UintInput("Введите желаемое количество столбцов массива: ");
+
+float[,] newArray = CreateRandom2DArrayF(m, n);
+Console.WriteLine();
+Console.WriteLine("Ваш сгенерированный массив: ");
+Print2DArrayF(newArray);
+Console.WriteLine();
